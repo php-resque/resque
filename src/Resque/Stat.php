b31dbffadc354e00b1edcb4a67ad39245f6e4e9a
Paul Croker <paul@zomble.com.au>
2014-09-02 22:29:09 +1000

    fixed E_STRICT issue. Resque_Stat methods are now marked as static, and now mark as deprecated

--- src/Resque/Stat.php
+++ src/Resque/Stat.php
@@ -5,6 +5,8 @@ namespace Resque;
 /**
  * Resque statistic management (jobs processed, failed, etc)
  *
+ * @deprecated on its way out the door.
+ *
  * @package		Resque/Stat
  * @author		Chris Boulton <chris@bigcommerce.com>
  * @license		http://www.opensource.org/licenses/mit-license.php
@@ -17,7 +19,7 @@ class Stat
 	 * @param string $stat The name of the statistic to get the stats for.
 	 * @return mixed Value of the statistic.
 	 */
-	public function get($stat)
+	public static function get($stat)
 	{
 		return (int)Resque::redis()->get('stat:' . $stat);
 	}
@@ -29,7 +31,7 @@ class Stat
 	 * @param int $by The amount to increment the statistic by.
 	 * @return boolean True if successful, false if not.
 	 */
-	public function incr($stat, $by = 1)
+	public static function incr($stat, $by = 1)
 	{
 		return (bool)Resque::redis()->incrby('stat:' . $stat, $by);
 	}
@@ -41,7 +43,7 @@ class Stat
 	 * @param int $by The amount to decrement the statistic by.
 	 * @return boolean True if successful, false if not.
 	 */
-	public function decr($stat, $by = 1)
+	public static function decr($stat, $by = 1)
 	{
 		return (bool)Resque::redis()->decrby('stat:' . $stat, $by);
 	}
@@ -52,7 +54,7 @@ class Stat
 	 * @param string $stat The name of the statistic to delete.
 	 * @return boolean True if successful, false if not.
 	 */
-	public function clear($stat)
+	public static function clear($stat)
 	{
 		return (bool)Resque::redis()->del('stat:' . $stat);
 	}
