76f1d71050e37569feb0e74f38faf6f568298cc0
Paul Croker <paul@zomble.com.au>
2014-09-29 21:03:16 +1000

    improved and restored worker event tests

--- src/Resque/Tests/EventTest.php
+++ src/Resque/Tests/EventTest.php
@@ -4,81 +4,9 @@ namespace Resque\Tests;
 
 use Resque\Job;
 use Resque\Resque;
-use Resque\Foreman;
-use Resque\Queue;
-use Resque\Worker;
 
 class EventTest extends ResqueTestCase
 {
-    private $callbacksHit = array();
-
-    public function setUp()
-    {
-//		Test_Job::$called = false;
-//
-//		// Register a worker to test with
-//		$this->worker = new \Resque\Worker(new \Resque\Foreman(), 'jobs');
-//		//$this->worker->setLogger(new Resque_Log());
-//		//$this->worker->registerWorker();
-    }
-
-    public function tearDown()
-    {
-        return self::markTestSkipped();
-
-        \Resque\Event::clearListeners();
-        $this->callbacksHit = array();
-    }
-
-    public function getEventTestJob()
-    {
-        return self::markTestSkipped();
-
-        $payload = array(
-            'class' => 'Test_Job',
-            'args' => array(
-                'somevar',
-            ),
-        );
-        $job = new Resque_Job('jobs', $payload);
-        $job->worker = $this->worker;
-        return $job;
-    }
-
-    public function testBeforeForkEventCallbackFires()
-    {
-        return self::markTestSkipped();
-
-        $event = 'beforeFork';
-        $callback = 'beforeForkEventCallback';
-
-        \Resque\Event::listen($event, array($this, $callback));
-        Resque::enqueue(
-            'jobs',
-            'Test_Job',
-            array(
-                'somevar'
-            )
-        );
-        $job = $this->getEventTestJob();
-        $this->worker->work(0);
-        $this->assertContains($callback, $this->callbacksHit, $event . ' callback (' . $callback . ') was not called');
-    }
-
-    public function testBeforePerformEventCanStopWork()
-    {
-        return self::markTestSkipped();
-
-        $callback = 'beforePerformEventDontPerformCallback';
-        \Resque\Event::listen('beforePerform', array($this, $callback));
-
-        $job = $this->getEventTestJob();
-
-        $this->assertFalse($job->perform());
-        $this->assertContains($callback, $this->callbacksHit, $callback . ' callback was not called');
-        $this->assertFalse(Test_Job::$called, 'Job was still performed though Resque_Job_DontPerform was thrown');
-    }
-
     public function testAfterEnqueueEventCallbackFires()
     {
         return self::markTestSkipped();
@@ -96,28 +24,6 @@ class EventTest extends ResqueTestCase
         );
         $this->assertContains($callback, $this->callbacksHit, $event . ' callback (' . $callback . ') was not called');
     }
-
-    public function testStopListeningRemovesListener()
-    {
-        return self::markTestSkipped();
-
-        $callback = 'beforePerformEventCallback';
-        $event = 'beforePerform';
-
-        \Resque\Event::listen($event, array($this, $callback));
-        \Resque\Event::stopListening($event, array($this, $callback));
-
-        $job = $this->getEventTestJob();
-        $this->worker->perform($job);
-        $this->worker->work(0);
-
-        $this->assertNotContains(
-            $callback,
-            $this->callbacksHit,
-            $event . ' callback (' . $callback . ') was called though \Resque\Event::stopListening was called'
-        );
-    }
-
 
     public function beforePerformEventDontPerformCallback($instance)
     {
