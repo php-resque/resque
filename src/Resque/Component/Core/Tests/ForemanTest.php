3904f3cce069893a1c4b9d63eb184d466d73a0ec
Paul Croker <paul@zomble.com.au>
2015-06-16 19:40:05 +1000

    Fixed specs and added fork to work test for the Foreman. Fixes #6

--- src/Resque/Component/Core/Tests/ForemanTest.php
+++ src/Resque/Component/Core/Tests/ForemanTest.php
@@ -2,64 +2,25 @@
 
 namespace Resque\Component\Core\Tests;
 
+use PHPUnit_Framework_TestCase;
 use Resque\Component\Core\Foreman;
-use Resque\Redis\RedisWorkerRegistry;
-use Resque\Component\Core\Test\ResqueTestCase;
-use Resque\Component\Worker\Factory\WorkerFactory;
 
-class ForemanTest extends ResqueTestCase
+class ForemanTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Foreman
-     */
-    protected $foreman;
-
-    /**
-     * @var \Resque\Redis\RedisWorkerRegistry
-     */
-    protected $workerRegistry;
-
-    public function testForking()
+    public function testForkToWork()
     {
-
-        $this->workerRegistry = new RedisWorkerRegistry($this->redis, $this->getMock('Resque\Component\Worker\Factory\WorkerFactoryInterface'));
-        $this->foreman = new Foreman($this->workerRegistry);
-
-
-        function it_starts_workers(
-            WorkerInterface $worker1,
-            WorkerInterface $worker2,
-            WorkerInterface $worker3,
-            EventDispatcherInterface $eventDispatcher
-        ) {
-            $eventDispatcher->dispatch(ResqueEvents::BEFORE_FORK, null)->shouldBeCalled(3);
-
-            $worker1->work()->shouldBeCalled();
-            $worker1->setProcess(Argument::type('Resque\Component\Core\Process'))->shouldBeCalled();
-            $worker1->getId()->shouldBeCalled()->willReturn('earth:1:lunch');
-            $worker2->work()->shouldBeCalled();
-            $worker2->setProcess(Argument::type('Resque\Component\Core\Process'))->shouldBeCalled();
-            $worker2->getId()->shouldBeCalled()->willReturn('earth:2:high');
-            $worker3->work()->shouldBeCalled();
-            $worker3->setProcess(Argument::type('Resque\Component\Core\Process'))->shouldBeCalled();
-            $worker3->getId()->shouldBeCalled()->willReturn('earth:3:test');
-
-            $workers = array(
-                $worker1,
-                $worker2,
-                $worker3
-            );
-
-            $this->work($workers);
-        }
-
         $me = getmypid();
+        $workerRegistry = $this->getMock('Resque\Component\Worker\Registry\WorkerRegistryInterface');
+        $jobInstanceFactory = $this->getMock('Resque\Component\Job\Factory\JobInstanceFactoryInterface');
+        $eventDispatcher = $this->getMock('Resque\Component\Core\Event\EventDispatcherInterface');
+        $foreman = new Foreman($workerRegistry, $eventDispatcher);
 
         $mockWorker = $this->getMock(
             'Resque\Component\Worker\Worker',
             array('work'),
-            array(array())
+            array($jobInstanceFactory, $eventDispatcher)
         );
+
         $mockWorker
             ->expects($this->any())
             ->method('work')
@@ -69,11 +30,9 @@ class ForemanTest extends ResqueTestCase
             clone $mockWorker,
             clone $mockWorker,
             clone $mockWorker,
-            clone $mockWorker,
-            clone $mockWorker,
         );
 
-        $this->foreman->work($workers, true);
+        $foreman->work($workers, true);
 
         // Check the workers hold different PIDs
         foreach ($workers as $worker) {
--- src/Resque/Component/Core/spec/Resque/Component/Core/ForemanSpec.php
+++ src/Resque/Component/Core/spec/Resque/Component/Core/ForemanSpec.php
@@ -24,7 +24,8 @@ class ForemanSpec extends ObjectBehavior
         $this->shouldHaveType('Resque\Component\Core\Foreman');
     }
 
-    // $this->work is tested with phpunit, due to process forking. I can't work out how to do it sanely in phpspec.
+    // $this->work is tested with phpunit, due to process forking. I can't work out how to spec it in phpspec.
+    // @see Resque\Component\Core\Tests\ForemanTest
 
     function it_cleans_up_dead_workers(
         WorkerRegistryInterface $workerRegistry,
@@ -35,15 +36,16 @@ class ForemanSpec extends ObjectBehavior
         Process $deadWorker1Process,
         Process $deadWorker2Process
     ) {
+        $hostname = (function_exists('gethostname')) ? gethostname() : php_uname('n'); // @todo fix hostname fetching
         $workerRegistry->all()->shouldBeCalled()->willReturn(array($worker, $deadWorker1, $deadWorker2));
 
-        $worker->getId()->shouldBeCalled()->willReturn('localhost:50:jobs');
+        $worker->getHostname()->shouldBeCalled()->willReturn($hostname);
         $worker->getProcess()->shouldBeCalled()->willReturn($workerProcess);
-        $workerProcess->getPid()->shouldBeCalled()->willReturn('50');
-        $deadWorker1->getId()->shouldBeCalled()->willReturn('localhost:1:jobs');
+        $workerProcess->getPid()->shouldBeCalled()->willReturn(getmypid());
+        $deadWorker1->getHostname()->shouldBeCalled()->willReturn($hostname);
         $deadWorker1->getProcess()->shouldBeCalled()->willReturn($deadWorker1Process);
         $deadWorker1Process->getPid()->shouldBeCalled()->willReturn('1');
-        $deadWorker2->getId()->shouldBeCalled()->willReturn('localhost:2:high,low');
+        $deadWorker2->getHostname()->shouldBeCalled()->willReturn($hostname);
         $deadWorker2->getProcess()->shouldBeCalled()->willReturn($deadWorker2Process);
         $deadWorker2Process->getPid()->shouldBeCalled()->willReturn('2');
 
@@ -57,12 +59,20 @@ class ForemanSpec extends ObjectBehavior
     function it_does_not_clean_up_workers_on_another_host(
         $workerRegistry,
         WorkerInterface $localWorker,
-        WorkerInterface $remoteWorker
+        WorkerInterface $remoteWorker,
+        Process $localProcess,
+        Process $remoteProcess
     ) {
         $workerRegistry->all()->shouldBeCalled()->willReturn([$localWorker, $remoteWorker]);
 
-        $localWorker->getId()->shouldBeCalled()->willReturn('localhost:1:jobs'); // @todo deal with hostname
-        $remoteWorker->getId()->shouldBeCalled()->willReturn('my.other.host:1:jobs');
+        $localWorker->getProcess()->shouldBeCalled()->willReturn($localProcess);
+        $remoteWorker->getProcess()->shouldBeCalled()->willReturn($remoteProcess);
+
+        $localWorker->getHostname()->shouldBeCalled()->willReturn((function_exists('gethostname')) ? gethostname() : php_uname('n')); // @todo fix hostname fetching
+        $remoteWorker->getHostname()->shouldBeCalled()->willReturn('my.other.host');
+
+        $localProcess->getPid()->willReturn(1);
+        $remoteProcess->getPid()->willReturn(1);
 
         $workerRegistry->deregister($localWorker)->shouldBeCalled();
         $workerRegistry->deregister($remoteWorker)->shouldNotBeCalled();
